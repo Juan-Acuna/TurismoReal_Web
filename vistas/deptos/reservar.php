9235523a653b2ce3a7f097802b993ef18b7a3709
Juan Acuña <unusuario256@hotmail.com>
2020-10-30 20:26:41 -0300

    validador fecha (parte 1)

--- vistas/deptos/reservar.php
+++ vistas/deptos/reservar.php
@@ -19,28 +19,62 @@
         <!-- Core theme CSS (includes Bootstrap)-->
         <script src="<?php echo BOOSTRAP;?>/js/boostrap.js"></script>
         <link href="<?php echo CSS;?>/styles.css" rel="stylesheet" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        
         <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900|Oswald:300,400,700" rel="stylesheet">
     <link href="<?php echo CSS;?>/styles.css" rel="stylesheet" />
         <link href="<?php echo CSS;?>/stylish-portfolio.min.css" rel="stylesheet">  
     <link rel="stylesheet" href="<?php echo CSS;?>/compradepa.css">
+    <script src="http://momentjs.com/downloads/moment.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <script>
-function restaFechas(f1,f2)
- {
- var aFecha1 = f1.split('/');
- var aFecha2 = f2.split('/');
- var fFecha1 = Date.UTC(aFecha1[2],aFecha1[1]-1,aFecha1[0]);
- var fFecha2 = Date.UTC(aFecha2[2],aFecha2[1]-1,aFecha2[0]);
- var dif = fFecha2 - fFecha1;
- var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
- return dias;
- }
-</script>
+
+    var dtI;
+    var dtF;
+    var costo;
+    var arriendo=0;
+    var dias;
+    function DiferenciaFechas(f1,f2)
+     {
+            var fecha1 = moment(f1);
+            var fecha2 = moment(f2);
+            return fecha2.diff(fecha1, 'days');
+     }
+     function CalcularEstadia(){
+        if(dtI.value!=null && dtF.value!=null){
+            var d = DiferenciaFechas(dtI.value,dtF.value);
+            if(d>=0){
+                costo.innerText="$"+arriendo*d;
+                if(d>0){
+                    dias.innerText="("+d+" Dias.)";
+                }else{
+                    dias.innerText="";
+                }
+            }
+        }
+    }
+    window.onload = function(){
+        dtI = document.getElementById("dtI");
+        dtF = document.getElementById("dtF");
+        costo = document.getElementById("costo");
+        dias = document.getElementById("dias");
+        dtI.min=moment(new Date()).add(1,'days').format("YYYY-MM-DD");
+        dtF.min=moment(new Date()).add(1,'days').format("YYYY-MM-DD");
+        dtI.addEventListener("change",function(){
+            var f = moment(dtI.value);
+            dtF.min=f.add(1,'days').format("YYYY-MM-DD");
+            CalcularEstadia();
+        });
+        dtF.addEventListener("change",function(){
+            var f = moment(dtF.value);
+            dtI.max=f.format("YYYY-MM-DD");
+            CalcularEstadia();
+        });
+    }
+    </script>
 <?php
         include "../../includes/navbar.php";
         include "../../funciones/peticion.php";
@@ -57,14 +91,6 @@ function restaFechas(f1,f2)
             echo '<header class="wrap">
     
             <section class="depa-list">
-        <script>
-        function CalcularEstadia(){
-            costo.innerText="$"+('.$depto['arriendo'].'*restaFechas(dtI.value,tdF.value));
-        }
-        function desbloquear(){
-            dtF.enabled=true;
-        }
-        </script>
                 <div class="depa-item" category="dep1">  
                
                     <img class="fotoaa" src="'.$actual.'" alt="aaa" width="510x700" >
@@ -76,9 +102,8 @@ function restaFechas(f1,f2)
                 </div>
                 
             </section> ';
-        
-    echo '<section class="depa-list">
-
+    echo '<script>arriendo='.$depto['arriendo'].'</script>
+    <section class="depa-list">
     <div class="depa-item" category="dep1">  
         <form class="formcompra">
             
@@ -86,14 +111,14 @@ function restaFechas(f1,f2)
                 <H1>Estadia</h1>
                 <div class="input-contenedor" >
                     
-                    <label>Fin Estadia</label>
-                    <input id="dtI" type="date" name="iestadia" onvaluechange="desbloquear()"/>
+                <label>Inicio Estadia</label>
+                    <input id="dtI" type="date" name="iestadia"/>
                     </br>
-                    <label>Inicio Estadia</label>
-
-                    <input id="dtF" type="date" name="festadia" />
+                    
+                    <label>Fin Estadia</label>
+                    <input id="dtF" type="date" name="festadia"/>
                 </div>
-                <h2 style="display:inline-block; margin-left:10%;">Costo Estadia: <span id="costo">$0</span></h2>
+                <h2 style="display:inline-block; margin-left:10%;">Costo Estadia: <span id="costo">$0</span> <span id="dias"></span></h2>
 
             </div>
 </section>
@@ -163,18 +188,4 @@ function restaFechas(f1,f2)
 }
 }
 ?>
-<script>
-var dtI = document.getElementById("dtI");
-var dtF = document.getElementById("dtF");
-var costo = document.getElementById("costo");
-$(document).ready(function(){
-    
-    $('#dtI').on('changeDate', function (ev) {
-        CalcularEstadia();
-    });
-    $('#dtF').on('changeDate', function (ev) {
-        CalcularEstadia();
-    });
-});
-</script>
 </body>
