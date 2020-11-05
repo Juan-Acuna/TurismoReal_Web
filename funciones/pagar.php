11ff5b467d4b4ac698729d5d5d8fa9ce99cea912
Juan Acuña <unusuario256@hotmail.com>
2020-11-05 00:20:51 -0300

    reserva en proceso*

--- funciones/pagar.php
+++ funciones/pagar.php
@@ -6,42 +6,54 @@ switch($_SERVER['REQUEST_METHOD']){
     case 'POST':
         switch($_POST['act']){
             case 'pay':
+                $apagar=0;
                 $dep = json_decode($_POST['depto'],true);
                 $reserva = json_decode($_POST['reserva'],true);
-                $total = intval($_POST['total']);
-                $res = peticion_http('http://turismoreal.xyz/api/transaccion'.$reserva['id_reserva'],'GET','',$_COOKIE['token']);
-                $pagos=1;
-                $pagos2=1;
-                $n=round($total/2);
-                $n2=$total-$n;
-                if($n>50000){
-                    do{
-                        $r=$n-(50000*$pagos);
-                        $pagos = $pagos + 1;
-                    }while($r>50000);
-                    if($n2>50000){
-                        do{
-                            $r=$n2-(50000*$pagos2);
-                            $pagos2 = $pagos2 + 1;
-                        }while($r>50000);
-                    }
-                }
+                $total = 0;
+                $res = peticion_http('http://turismoreal.xyz/api/transaccion'.$reserva['contenido']['id_reserva'],'GET','',$_COOKIE['token']);
+                if($res['statusCode']==200){
+                    if($res['contenido']['id_estado']==1){
+                        if($res['contenido']['n_pagos']<$res['contenido']['pagos']){
+                            $total=$res['contenido']['valor_total'];
+                            $pagos=1;
+                            $pagos2=1;
+                            $n=round($total/2);
+                            $n2=$total-$n;
+                            if($n>50000){
+                                do{
+                                    $r=$n-(50000*$pagos);
+                                    $pagos = $pagos + 1;
+                                }while($r>50000);
+                                if($n2>50000){
+                                    do{
+                                        $r=$n2-(50000*$pagos2);
+                                        $pagos2 = $pagos2 + 1;
+                                    }while($r>50000);
+                                }
+                            }
+                            
+                            $transaccion = array(
+                                "Monto"=>$apagar,
+                                "Comentario"=>"Arriendo Depto. ".$dep['contenido']['nombre'].".",
+                                "Username"=>$_COOKIE['username'],
+                                "Id_reserva"=>$reserva['contenido']['id_reserva'],
+                                "Id_tipo"=>1
+                            );
+                            $pago = peticion_http('http://turismoreal.xyz/api/transaccion','POST',$transaccion,$_COOKIE['token']);
+                            if($pago['statusCode']==200){
+                                header('Location: '.$pago['contenido']['payment_url']);
+                                die();
+                            }else{
+                                echo 'Error en transaccion';
+                                echo $pago['statusText'].' - ';
+                                var_dump($pago['contenido']);
+                            }
+                        }
+                        
+                    }else if($res['contenido']['id_estado']==2){
 
-                $transaccion = array(
-                    "Monto"=>round($total/4),
-                    "Comentario"=>"Arriendo Depto. ".$dep['contenido']['nombre'].".",
-                    "Username"=>$_COOKIE['username'],
-                    "Id_reserva"=>$reserva['contenido']['id_reserva'],
-                    "Id_tipo"=>1
-                );
-                $pago = peticion_http('http://turismoreal.xyz/api/transaccion','POST',$transaccion,$_COOKIE['token']);
-                if($pago['statusCode']==200){
-                    header('Location: '.$pago['contenido']['payment_url']);
-                    die();
-                }else{
-                    echo 'Error en transaccion';
-                    echo $pago['statusText'].' - ';
-                    var_dump($pago['contenido']);
+                    }
+                    
                 }
             break;
             case '':
